f22520d3ca2d2bc923a5ebce464153c819af1128
daveancheta <daveancheta8@gmail.com>
2026-03-18 20:59:57 +0800

    feat(auth): implement GitHub social sign-in functionality

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,9 @@
 import { createAuthClient } from "better-auth/react" // make sure to import from better-auth/react
 
-export const authClient =  createAuthClient({
-    //you can pass client configuration here
-})
+export const authClient =  createAuthClient()
+
+export const signIn = async () => {
+    const data = await authClient.signIn.social({
+        provider: "github"
+    })
+}
