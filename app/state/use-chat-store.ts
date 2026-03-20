83a861364a64ba07c11b3a42a01c39add2c76ccd
daveancheta <daveancheta8@gmail.com>
2026-03-20 16:24:00 +0800

    feat(chat): define Messages interface and initialize messages state in chat store

--- app/state/use-chat-store.ts
+++ app/state/use-chat-store.ts
@@ -1,19 +1,52 @@
 import { create } from "zustand";
 
+interface Messages {
+    id: string;
+    user_id: string;
+    group_id: string;
+    created_at: string;
+    string: string;
+    group: {
+        id: string;
+        photo: string;
+        created_at: string;
+        created_by: string;
+        group_name: string;
+        updated_at: string;
+    },
+    user: {
+        id: string;
+        name: string;
+        email: string;
+        image: string;
+        birthdate: string;
+        created_at: string;
+        updated_at: string;
+        email_verified: string;
+    }
+}
+
 interface ChatState {
     selectedTeam: string | null;
+    messages: Messages[];
     setSelectedTeam: (selectedTeam: string) => void;
     handleGetMessages: (id: string) => Promise<void>;
 
 }
+
 export const UseChatStore = create<ChatState>((set) => ({
     selectedTeam: null,
+    messages: [],
 
     setSelectedTeam: (selectedTeam: string) => set({ selectedTeam: selectedTeam }),
 
     handleGetMessages: async (id: string) => {
         try {
             const messages = await fetch(`/api/chat/${id}`)
+
+            const res = await messages.json()
+
+            set({ messages: res.message })
         } catch (error) {
             console.log(error)
         }
