232a9c935d837b1a69f2eef787c007758072eb54
daveancheta <daveancheta8@gmail.com>
2026-03-20 14:18:20 +0800

    feat(group): implement GET endpoint to retrieve member data with group details

--- app/api/group/route.ts
+++ app/api/group/route.ts
@@ -57,3 +57,37 @@ export async function POST(req: NextRequest) {
         }, { status: 400 })
     }
 }
+
+export async function GET() {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if (!session) {
+        return NextResponse.json({
+            success: false,
+            message: "Unauthorized. Please sign in to continue."
+        }, { status: 400 })
+    }
+
+    try {
+        const { data, error } = await supabase
+            .from('members')
+            .select(`*,
+                group (*)
+                `)
+            .eq('user_id', session.user.id)
+
+        return NextResponse.json({
+            success: true,
+            data,
+            error
+        }, { status: 200 })
+    } catch (error) {
+        console.log(error)
+
+        return NextResponse.json({
+            success: false,
+        }, { status: 400 })
+    }
+}
