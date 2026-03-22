4688f4ba1abf1463c13a9b1de379808656be14f4
daveancheta <daveancheta8@gmail.com>
2026-03-22 11:42:16 +0800

    feat: add API endpoint to fetch group members by group ID

---
+++ app/api/group/members/[id]/route.ts
@@ -0,0 +1,27 @@
+import { supabase } from "@/lib/supbase/cient";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params
+
+    try {
+        const { data, error } = await supabase
+            .from('members')
+            .select(`*, 
+                group (*), 
+                user (*)`)
+            .eq('group_id', id)
+
+        return NextResponse.json({
+            success: true,
+            data,
+            error
+        }, { status: 200 })
+    } catch (error) {
+        console.log(error)
+        
+        return NextResponse.json({
+            success: false
+        }, { status: 400 })
+    }
+}
