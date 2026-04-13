c83077e83d638d5a18348798b31732f20188fb6d
daveancheta <daveancheta8@gmail.com>
2026-04-13 18:10:27 +0800

    feat: add new notification format inm added new task

--- app/api/group/task/route.ts
+++ app/api/group/task/route.ts
@@ -1,8 +1,21 @@
+import { auth } from "@/lib/auth";
 import { supabase } from "@/lib/supbase/cient";
+import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     const { user_id, group_id, description } = await req.json()
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if (!session ) {
+        return NextResponse.json({
+            success: false,
+            message: "Unauthorized. Please sign in to continue."
+        }, { status: 400 })
+    }
 
     try {
         const { data, error } = await supabase
@@ -19,7 +32,7 @@ export async function POST(req: NextRequest) {
         .insert({
             id: crypto.randomUUID(),
             user_id,
-            message: `New task added: ${description}`,
+            message: `${session.user.name.split(" ")[0]} assigned you a new task: "${description}"`,
         })
 
         return NextResponse.json({
