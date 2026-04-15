a978ed0bcdd1ae7d194855788c9b44d5d03e310d
daveancheta <daveancheta8@gmail.com>
2026-04-15 13:43:10 +0800

    style: update loading state UI for team section

--- components/team-state.tsx
+++ components/team-state.tsx
@@ -1,4 +1,3 @@
-import { Loader2 } from 'lucide-react'
 import React from 'react'
 import { Skeleton } from './ui/skeleton'
 
@@ -6,11 +5,20 @@ function TeamState() {
   return (
     <div className='flex flex-col gap-4'>
       {Array.from({ length: 10 }).map((_, index) =>
-        <div key={index}>
-          <Skeleton className='w-full h-14' />
+        <div className="flex flex-col gap-2 rounded-lg border p-3" key={index}>
+            <div className="flex items-center gap-3">
+                <Skeleton className="rounded-full w-10 h-10 shrink-0" />
+                <div className="flex flex-1 flex-col gap-1.5 py-1">
+                   <div className='flex justify-between'>
+                     <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-12" />
+                   </div>
+
+                    <Skeleton className="h-3 w-24" />
+                </div>
+            </div>
         </div>
-      )
-      }
+      )}
     </div>
   )
 }
